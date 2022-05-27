7143ebaa93fd784c0b23fe71736d264ecc890faf
skwits-dev <skwitz.dev@gmail.com>
2022-05-27 18:00:04 -0400

    Add simpleMultiplication.js

---
+++ simpleMultiplication.js
@@ -0,0 +1,11 @@
+//simple multiplication -- if number even, return 8; otherwise return 9
+
+function simpleMultiplication(number) {
+    if ((number%2)==0) {
+      return (number * 8)
+      
+    }
+  else {
+    return (number * 9);
+  }
+}
