350a208b2258f2f1171297c0b57d9e626d7d32dc
skwits-dev <skwitz.dev@gmail.com>
2022-05-28 22:13:49 -0400

    Add boolToString.js

---
+++ boolToString.js
@@ -0,0 +1,6 @@
+/* implement a function, which converts the given boolean value to its
+string representation. Note: Only valid inputs will be given.*/
+
+function booleanToString(b){
+    return b.toString("");
+  }
